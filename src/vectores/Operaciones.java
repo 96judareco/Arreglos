dd4ec612fcfe44333172add1ccc81e0a33bb2b19
96judareco <delben555@gmail.com>
2017-08-28 21:02:36 -0500

    commit inicial

---
+++ src/vectores/Operaciones.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vectores;
+
+/**
+ * Este proyecto Genera vectores y matrices con n numeros de elementos
+ * @author Juan Ricardo Rodriguez Campos
+ * @author Juan David Reyes
+ * @verion 1.0.0
+ * @since Generador Vectores y Matrices 1.0.0
+ */
+public interface Operaciones {
+    public void ascendete();
+    public void descendente();
+}
